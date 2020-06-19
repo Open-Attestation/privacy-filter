6b52c3f13cbc84940e65102fdd67f018b6d6c828
Nebulis <nebounet@gmail.com>
2020-06-19 17:39:07 +0800

    fix: it test

--- integration/integration.spec.ts
+++ integration/integration.spec.ts
@@ -2,18 +2,8 @@ import { Selector } from "testcafe";
 
 fixture("Counter Game").page`http://localhost:3000`;
 
-const Counter = Selector(".counter div").nth(0);
-const Increment = Selector(".counter button").nth(0);
-const Decrement = Selector(".counter button").nth(1);
+const Header = Selector("h1").nth(0);
 
 test("Custom certificate is rendered correctly", async (t) => {
-  await t.expect(Counter.textContent).eql("Counter: 0");
-  await t.click(Increment);
-  await t.click(Increment);
-  await t.click(Increment);
-  await t.click(Increment);
-  await t.expect(Counter.textContent).eql("Counter: 4");
-  await t.click(Decrement);
-  await t.click(Decrement);
-  await t.expect(Counter.textContent).eql("Counter: 2");
+  await t.expect(Header.textContent).contains("OpenAttestation Privacy Filter");
 });
