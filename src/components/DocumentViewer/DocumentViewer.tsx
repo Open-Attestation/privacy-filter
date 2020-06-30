879f171a2444fad42108f83b915d5dbb76239945
Goi Jia Jian <jiajiannn@gmail.com>
2020-07-01 02:01:47 +0800

    feat: hooks + download btn

--- src/components/DocumentViewer/DocumentViewer.tsx
+++ src/components/DocumentViewer/DocumentViewer.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import { obfuscateDocument, getData } from "@govtechsg/open-attestation";
+import { saveAs } from "file-saver";
+import React, { useState, useEffect } from "react";
 import { RecommendationsDisplay } from "../RecommendationsDisplay";
 import { findAllSensitiveFields } from "../SensitiveFieldsFinder";
 import { flatten } from "../shared";
@@ -12,20 +14,10 @@ interface ButtonProps {
   path: string;
 }
 
-export const DocumentViewer: React.FunctionComponent<DocumentViewerProps> = ({ document }) => {
-  // const download = () => {
-  //   const redacted = obfuscateDocument(document, sensitiveFields);
-  //   const blob = new Blob([JSON.stringify(redacted, null, 2)], {
-  //     type: "application/json",
-  //   });
-  //   saveAs(blob, fileName);
-  // };
-
+export const DocumentViewer: React.FunctionComponent<DocumentViewerProps> = ({ document, fileName }) => {
   const [redactionList, setRedactionList] = useState<string[]>([]);
   const toggleChoice = (path: string): void => {
     const _redactionList: string[] = [...redactionList];
-    console.log(path);
-
     const index = _redactionList.indexOf(path, 0);
     if (index > -1) {
       _redactionList.splice(index, 1);
@@ -34,7 +26,19 @@ export const DocumentViewer: React.FunctionComponent<DocumentViewerProps> = ({ d
     }
     // console.log(_redactionList);
     setRedactionList(_redactionList);
-    console.log(redactionList);
+  };
+  useEffect(() => {
+    console.log("redactionList:", redactionList);
+  });
+  const download = (): void => {
+    const verified = getData(document);
+    console.log(verified);
+    const redacted = obfuscateDocument(document, ["recipient.name"]);
+    console.log(redactionList, redacted);
+    const blob = new Blob([JSON.stringify(redacted, null, 2)], {
+      type: "application/json",
+    });
+    saveAs(blob, fileName);
   };
   const RedactButton: React.FunctionComponent<ButtonProps> = ({ path }) => {
     return (
@@ -52,7 +56,7 @@ export const DocumentViewer: React.FunctionComponent<DocumentViewerProps> = ({ d
         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => toggleChoice(path)}
       >
-        Undo Redact
+        Undo
       </button>
     );
   };
@@ -61,11 +65,27 @@ export const DocumentViewer: React.FunctionComponent<DocumentViewerProps> = ({ d
     const sensitiveFields = findAllSensitiveFields(data);
     const hasSensitiveFields = sensitiveFields.length > 0;
 
+    if (hasSensitiveFields) {
+      // const _redactionList: string[] = [...redactionList];
+      // sensitiveFields.map((row: { path: string }) => {
+      //   _redactionList.push(row.path);
+      // });
+      // setRedactionList(_redactionList);
+    }
+
     return (
       <>
         <div className="bg-gray-300 font-bold rounded-t px-4 py-2">Document Viewer</div>
         <div className="border border-t-0 border-gray-200 rounded-b px-4 py-3 overflow-auto">
           <RecommendationsDisplay hasSensitiveFields={hasSensitiveFields} />
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => download()}
+          >
+            Redact
+          </button>
+
           <table className="table-fixed w-full">
             <thead>
               <tr>
@@ -76,26 +96,21 @@ export const DocumentViewer: React.FunctionComponent<DocumentViewerProps> = ({ d
             </thead>
             <tbody>
               {data.map((row) => {
-                // if (sensitiveFields.includes(row)) {
-                //   toggleChoice(row.path as string);
-                // }
-
-                // <mark className="line-through">{row.path}</mark>
-
-                const button = redactionList.includes(row.path as string) ? (
-                  <UndoRedactButton path={row.path as string} />
-                ) : (
-                  <RedactButton path={row.path as string} />
-                );
+                let button, path, value;
+                if (redactionList.includes(row.path as string)) {
+                  button = <UndoRedactButton path={row.path as string} />;
+                  path = <p className="line-through break-words mark">{row.path}</p>;
+                  value = <p className="line-through truncate mark">{row.value}</p>;
+                } else {
+                  button = <RedactButton path={row.path as string} />;
+                  path = <p className="break-words">{row.path}</p>;
+                  value = <p className="truncate">{row.value}</p>;
+                }
 
                 return (
                   <tr key={row.path}>
-                    <td className="border px-2 py-2">
-                      <p className="break-words">{row.path}</p>
-                    </td>
-                    <td className="border px-2 py-2">
-                      <p className="truncate">{row.value}</p>
-                    </td>
+                    <td className="border px-2 py-2">{path}</td>
+                    <td className="border px-2 py-2">{value}</td>
                     <td className="border px-2 py-2">{button}</td>
                   </tr>
                 );
